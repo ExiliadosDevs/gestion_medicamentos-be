e635031965d7f4943fee422b1486e94c7eb8ac21
juann19 <juandavidsalcedo123@gmail.com>
2025-02-10 12:17:38 -0500

    Primer push -backend iniciado

---
+++ src/main/java/com/verionpruebas/version_pruebas_medicamentos_back/VersionPruebasMedicamentosBackApplication.java
@@ -0,0 +1,13 @@
+package com.verionpruebas.version_pruebas_medicamentos_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersionPruebasMedicamentosBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersionPruebasMedicamentosBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/verionpruebas/version_pruebas_medicamentos_back/VersionPruebasMedicamentosBackApplicationTests.java
@@ -0,0 +1,14 @@
+package com.verionpruebas.version_pruebas_medicamentos_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersionPruebasMedicamentosBackApplicationTests {
+
+    @Test
+    @org.junit.jupiter.api.Disabled // Esto evita que el test se ejecute
+	void contextLoads() {
+	}
+
+}
